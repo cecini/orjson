d0cdbfc1badd670f0655dc4006c91a5678dc01d9
cecini <loleta842jhu@gmail.com>
2020-11-22 09:05:57 +0000

    testor

--- testor.py
+++ testor.py
@@ -1,7 +1,23 @@
+#import pdb;pdb.set_trace()
+# should not put when using run bazel 
+import gc 
+#gc.set_debug(gc.DEBUG_LEAK)
+gc.disable()
+import tracemalloc
+tracemalloc.start(25)
+gc.enable()
 import orjson
+gc.disable()
+
 import time
+# PYTHONMALLOCSTATS=1
+# PYTHONTRACEMALLOC=25
 print("I have loaeded the orjson library")
 
-time.sleep(10)
+time.sleep(2)
 print("I have slept 10s, exit, done!")
-to_json_bytes = orjson.dumps
+#gc.set_debug(gc.DEBUG_LEAK)
+#to_json_bytes = orjson.dumps
+orjson.dumps([])
+
+print("I have done!")
